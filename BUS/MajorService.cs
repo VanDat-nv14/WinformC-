1d64a03f5d64310d30c0d0bac460df97a16fc764
Nguyễn Văn Đạt <nvdat14012004@gmail.com>
2024-10-17 21:56:45 +0700

    Add files via upload

---
+++ BUS/MajorService.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class MajorService
+    {
+        public List<Major> GetAllByFacylty(int facultyID)
+        {
+            StudentcontextDB context = new StudentcontextDB(); 
+            return context.Majors.Where(p => p.FacultyID == facultyID).ToList();
+        }
+    }
+}
